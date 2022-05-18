dbfa30f8b0a1c959513ecfca95701f36372a9124
Krisostomos Abdi <111201911695@mhs.dinus.ac.id>
2022-05-18 16:10:30 +0700

    Adding 2 methods to show recipe (paragraph and list). Currently using paragraph method.

--- food-data/main/food.js
+++ food-data/main/food.js
@@ -1,16 +1,24 @@
+/* eslint-disable max-len */
 const foods = [{
   id: 'friedrice',
   name: 'Fried Rice',
   description: 'Fried rice is a dish of cooked rice that has been stir-fried in a wok or a frying pan and is usually mixed with other ingredients such as eggs, vegetables, seafood, or meat. It is often eaten by itself or as an accompaniment ',
-  ingredients: ' ',
-  howtocook: ' ',
+  ingredients: 'Cooked and chilled rice, Eggs, Carrots, onions, green onions and peas, Garlic, Soy sauce, Oyster sauce, Toasted sesame oil, Butter',
+  howtocook: 'First, put about 6 cups of rice into your rice cooker. Let it steam until it is already cooked.Second, wash the vegetables, then dice the carrots and onions into small pieces. Set them aside for the next step. Third, add oil and heat up to 100 degrees. Fourth, toss the vegetables into the pan for about 3 minutes. Then toss in the carrots and onions for 3 minutes with the vegetables. After that add 1 teaspoon of salt into the pan. Fifth, boil the chicken or shrimp with the rest of the ingredients (optional). Sixth, put a bit more oil into the frying pan. And then toss the rice in carefully. Seventh, add an egg and mix it with the other ingredients. After that add approximately 2 to 3 tablespoons of soy sauce while frying. And for the last, put fried rice on a dish and it is ready to serve!',
+  // first: 'First, put about 6 cups of rice into your rice cooker. Let it steam until it is already cooked.',
+  // second: 'Second, wash the vegetables, then dice the carrots and onions into small pieces. Set them aside for the next step.',
+  // third: 'Third, add oil and heat up to 100 degrees.',
+  // fourth: 'Fourth, toss the vegetables into the pan for about 3 minutes. Then toss in the carrots and onions for 3 minutes with the vegetables. After that add 1 teaspoon of salt into the pan.',
+  // fifth: 'Fifth, boil the chicken or shrimp with the rest of the ingredients (optional).',
+  // sixth: 'Sixth, put a bit more oil into the frying pan. And then toss the rice in carefully.',
+  // seventh: 'Seventh, add an egg and mix it with the other ingredients. After that add approximately 2 to 3 tablespoons of soy sauce while frying.',
+  // eighth: 'And for the last, put fried rice on a dish and it is ready to serve!',
 },
 {
   id: 'sambalgoreng',
   name: 'Sambal Goreng',
   description: 'Sambal pedas di Goreng',
   ingredients: 'Cabe, Bawang, Penyedap',
-  howtocook: 'Digoreng',
 }];
 
 module.exports = foods;
