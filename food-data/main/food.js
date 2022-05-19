42133be8f8d46ca8352a92e749e87e44822b343a
CandhyFadhila <c2007f0683@bangkit.academy>
2022-05-19 12:22:00 +0700

    Change description *food.js

--- food-data/main/food.js
+++ food-data/main/food.js
@@ -17,18 +17,18 @@ const foods = [{
   // eighth: 'And for the last, put fried rice on a dish and it is ready to serve!',
 },
 {
-  id: 'ayamgulai',
-  name: 'Ayam Gulai',
-  description: 'Gulai is a type of food containing rich, spicy and succulent curry-like sauce commonly found in Indonesia, Malaysia and Singapore. The main ingredients of this dish are usually poultry, goat meat, beef, mutton, various kinds of offal, fish and seafood, as well as vegetables such as cassava leaves, unripe jackfruit and banana stem. Gulai is often described as Indonesian curry, although it is also considered a local dish in Malaysia and Singapore. Gulai is a common name to refer to curry dishes in the country, although Indonesian, Malaysian, and Singaporean cuisine also recognise kari (curry).',
-  ingredients: 'Cabe, Bawang, Penyedap',
-  howtocook: 'Digoreng',
+  id: '',
+  name: '',
+  description: '',
+  ingredients: '',
+  howtocook: '',
 },
 {
-  id: 'beefrendang',
-  name: 'Beef Rendang',
-  description: 'Rendang is a Minang dish originating from the Minangkabau region in West Sumatra, Indonesia. It has spread across Indonesian cuisine to the cuisines of neighbouring Southeast Asian countries such as Malaysia, Singapore, Brunei and the Philippines. Rendang is often described as a rich dish of meat that has been slow cooked and braised in a coconut milk seasoned with a herb and spice mixture, until the liquids evaporate and the meat turns dark brown and tender, becoming caramelized and infused with rich flavours.',
-  ingredients: 'Cabe, Bawang, Penyedap',
-  howtocook: 'Digoreng',
+  id: '',
+  name: '',
+  description: '',
+  ingredients: '',
+  howtocook: '',
 }];
 
 module.exports = foods;
